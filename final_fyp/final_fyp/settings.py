8bec9ff4040e5658f2f033e26608a7729b8b2c54
Chador Tenzin <122201230+12190001@users.noreply.github.com>
2023-06-09 10:33:49 +0600

    Update settings.py

--- final_fyp/final_fyp/settings.py
+++ final_fyp/final_fyp/settings.py
@@ -29,8 +29,8 @@ SECRET_KEY = 'django-insecure-!z^v!7u!-el8=uurnoar6nd%le6)+0_2&4hpm%(v!==20qpm2=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['gofoodie-1a86.onrender.com']
-CSRF_TRUSTED_ORIGINS = ['https://gofoodie-1a86.onrender.com']
+ALLOWED_HOSTS = ['gofoodie2.onrender.com']
+CSRF_TRUSTED_ORIGINS = ['https://gofoodie2.onrender.com']
 
 
 # Application definition
